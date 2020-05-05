32c39c3fbac7eb22db9e4a3f74392e1ada75f973
Pranet Verma <pranet@fb.com>
2020-05-04 22:13:55 -0700

    Break line in message log

--- src/MessageLog.js
+++ src/MessageLog.js
@@ -18,11 +18,12 @@ export class MessageLog extends React.Component<Props> {
 
   render() {
     return (
-      <div style={{overflow: 'auto', maxHeight: 400}}>
+      <div style={{overflow: 'auto', maxHeight: 300}}>
         <ReactList
           itemRenderer={this._renderMessageRow}
+          itemSizeGetter={this._getItemSize}
           length={this.props.messages.length}
-          type='uniform'
+          type='variable'
           ref={c => this.list = c}
         />
       </div>
@@ -30,7 +31,14 @@ export class MessageLog extends React.Component<Props> {
   }
 
   componentDidUpdate() {
-    this.list.scrollAround(this.props.messages.length - 1);     
+    this.list.scrollAround(this.props.messages.length- 1);
+  }
+
+  _getItemSize = (idx: number) => {
+    const len = this.props.messages[idx].length;
+    const ch_per_500_px = 82;
+    const lines = Math.ceil(len / ch_per_500_px);
+    return 18 * (lines);
   }
 
   _renderMessageRow = (idx: number, key: string) => {
